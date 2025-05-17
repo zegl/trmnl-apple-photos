2912d3768179910efc660d48b75883c701c8d00c
Gustav Westling <gustav@sanalabs.com>
2025-05-17 09:55:23 +0200

    Render all sizes

--- src/app/markup/route.tsx
+++ src/app/markup/route.tsx
@@ -4,20 +4,44 @@ export async function POST(request: Request) {
   // Extract data from POST request as form data
   const formData = await request.formData();
 
-  // Create URL with query parameters from form data
-  const url = new URL('/markup-get', request.url);
+  const sizes = [
+    {
+      json_name: 'markup',
+      size: 'full',
+    },
+    {
+      json_name: 'markup_half_vertical',
+      size: 'half_vertical',
+    },
+    {
+      json_name: 'markup_half_horizontal',
+      size: 'half_horizontal',
+    },
+    {
+      json_name: 'markup_quadrant',
+      size: 'quadrant',
+    },
+  ]
 
-  // Add each form field as a query parameter
-  for (const [key, value] of formData.entries()) {
-    url.searchParams.append(key, String(value));
+  const res = await Promise.all(sizes.map(async (size) => {
+    const url = new URL('/markup-get', request.url);
+    url.searchParams.append('size', size.size);
+    url.searchParams.append('user_uuid', formData.get('user_uuid') as string);
+
+    const response = await fetch(url, {
+      method: 'GET',
+    });
+
+    return {
+      json_name: size.json_name,
+      markup: await response.text(),
+    };
+  }));
+
+  let json: Record<string, string> = {};
+  for (const size of res) {
+    json[size.json_name] = size.markup;
   }
 
-  const response = await fetch(url, {
-    method: 'GET',
-  });
-
-  const responseBody = await response.text();
-  return NextResponse.json({
-    markup: responseBody,
-  });
+  return NextResponse.json(json);
 }
