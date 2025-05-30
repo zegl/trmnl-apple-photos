2ec3f77c7a1d4f6ce80c03a0e35a893acae81612
Gustav Westling <gustav@sanalabs.com>
2025-05-30 23:37:25 +0200

    worker

--- src/supabase.ts
+++ src/supabase.ts
@@ -2,8 +2,12 @@ import { createClient } from '@supabase/supabase-js';
 import jwt from 'jsonwebtoken';
 
 // These should be set in your environment variables
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_ANON_KEY || '';
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY || '';
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_ANON_KEY || '';
+const supabaseKey =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+  process.env.SUPABASE_ANON_KEY ||
+  '';
 const supabaseJwtSecret = process.env.SUPABASE_JWT_SECRET || '';
 
 if (!supabaseUrl) {
--- worker/main.ts
+++ worker/main.ts
@@ -1,3 +1,8 @@
+import {
+  fetchPublicAlbumWebAsset,
+  fetchPublicAlbumWebStream,
+  getPublicAlbumId,
+} from '@/app/apple-public-album';
 import { BlobRepository } from '@/blobs';
 import { getSupabaseClientForUser } from '@/supabase';
 import { Hatchet } from '@hatchet-dev/typescript-sdk';
@@ -5,21 +10,67 @@ import { Hatchet } from '@hatchet-dev/typescript-sdk';
 export const hatchet = Hatchet.init();
 
 export type RefreshAlbumInput = {
-  uuid: string;
+  user_uuid: string;
 };
 
 export const refreshAlbum = hatchet.task({
-  name: 'refreshAlbum',
+  name: 'trmnl-apple-photos:refresh-album',
   retries: 1,
   fn: async (input: RefreshAlbumInput) => {
-    const { uuid } = input;
-    console.log(`Refreshing album ${uuid}`);
+    const { user_uuid } = input;
+    console.log(`Refreshing album ${user_uuid}`);
 
-    const supabase = getSupabaseClientForUser(uuid);
+    const supabase = getSupabaseClientForUser(user_uuid);
 
     const blobRepository = new BlobRepository(supabase);
 
-    const userSettings = await blobRepository.getUserSettings(uuid);
+    const userSettings = await blobRepository.getUserSettings(user_uuid);
+    if (!userSettings) {
+      return {
+        success: false,
+        error: 'The album has not been set up yet.',
+      };
+    }
+
+    const getPartitionAndWebStreamResult =
+      await blobRepository.getPartitionAndWebStream(user_uuid);
+    let request_partition = 'p123';
+    if (
+      getPartitionAndWebStreamResult.success &&
+      getPartitionAndWebStreamResult.data.apple_partition
+    ) {
+      request_partition = getPartitionAndWebStreamResult.data.apple_partition;
+    }
+
+    const albumId = getPublicAlbumId(userSettings.sharedAlbumUrl);
+
+    const { webStream, partition } = await fetchPublicAlbumWebStream(
+      request_partition,
+      albumId
+    );
+
+    await blobRepository.setPartitionAndWebStream({
+      uuid: user_uuid,
+      apple_partition: partition,
+      web_stream_blob: webStream,
+    });
+
+    if (webStream.photos.length === 0) {
+      return {
+        success: false,
+        error: 'No photos found in the shared album. :-/',
+      };
+    }
+
+    // Crawl all images
+    for (const photo of webStream.photos) {
+      const webAsset = await fetchPublicAlbumWebAsset(
+        partition,
+        albumId,
+        photo.photoGuid
+      );
+      console.log({ guid: photo.photoGuid, webAsset });
+    }
 
     console.log(userSettings);
   },
@@ -33,7 +84,7 @@ async function main() {
     slots: 10,
   });
 
-  console.log("Starting worker")
+  console.log('Starting worker');
 
   await worker.start();
 }
