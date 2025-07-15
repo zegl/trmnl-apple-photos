b3dd1774196abc95d0ba19b696557b816827ad36
Gustav Westling <gustav@sanalabs.com>
2025-07-15 20:23:10 +0200

    Long-term routing middleware fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,22 +10,32 @@ export function middleware(request: NextRequest) {
 
   const devDefault: string = 'apple';
 
+
+  const isGoogleHost = request.nextUrl.hostname === 'trmnl-google-photos.westling.dev';
+  const isAppleHost = request.nextUrl.hostname === 'trmnl-apple-photos.westling.dev';
+  const isAnyHost = isGoogleHost || isAppleHost;
+
+  const fallbackToGoogle = !isAnyHost && devDefault === 'google';
+  const fallbackToApple = !isAnyHost && devDefault === 'apple';
+
+  const isGooglePhotosRoute = isRoute && (isGoogleHost || fallbackToGoogle);
+  const isApplePhotosRoute = isRoute && (isAppleHost || fallbackToApple);
+
+
   console.log('middleware', {
     isRoute,
     hostname: request.nextUrl.hostname,
     host: request.nextUrl.host,
+    isGoogleHost,
+    isAppleHost,
+    isAnyHost,
+    fallbackToGoogle,
+    fallbackToApple,
+    isGooglePhotosRoute,
+    isApplePhotosRoute,
   });
 
-  const isGooglePhotos =
-    isRoute &&
-    (request.nextUrl.hostname === 'trmnl-google-photos.westling.dev' ||
-      devDefault === 'google');
-  const isApplePhotos =
-    isRoute &&
-    (request.nextUrl.hostname === 'trmnl-apple-photos.westling.dev' ||
-      devDefault === 'apple');
-
-  if (isGooglePhotos) {
+  if (isGooglePhotosRoute) {
     console.log('rewriting to google', request.nextUrl.pathname);
     return NextResponse.rewrite(
       new URL(
@@ -35,7 +45,7 @@ export function middleware(request: NextRequest) {
     );
   }
 
-  if (isApplePhotos) {
+  if (isApplePhotosRoute) {
     console.log('rewriting to apple', request.nextUrl.pathname);
     return NextResponse.rewrite(
       new URL(
