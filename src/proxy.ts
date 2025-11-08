a1334eea41f751af0a7fe4e9edc0a3c2f35051a0
Gustav Westling <gustav@sanalabs.com>
2025-11-08 12:27:58 +0100

    Upgrade to Next 16

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const isRoute =
     !request.nextUrl.pathname.startsWith('/_next') &&
     !request.nextUrl.pathname.startsWith('/api') &&
